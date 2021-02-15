4a0d1803e2ae0ece4cab18407ded7e62a63ef478
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-02-15 23:19:12 +0100

    add hlint downloaded from server

---
+++ shlint.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function shlint --wraps='curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $argv --color | less -r' --description 'alias shlint curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $argv --color | less -r'
+  curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $argv --color | less -r;
+end
