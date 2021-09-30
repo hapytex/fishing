7007a765d5e0580017d5b99ba60cce1ac6f550c5
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-09-30 13:31:30 +0200

    fix install script and implement a powerline

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install apt-file fish rename git
+sudo apt-get install apt-file fish powerline rename
 
-pip3 --install --upgrade numpy pandas scipy sympy
+pip3 install --upgrade numpy pandas scipy sympy
 
 # load the apt cache to find out what package exposes a certain command
 sudo apt-file update
+
+cat >> ~/.config/fish/config.fish <<EOF
+
+set fish_function_path \$fish_function_path "/usr/share/powerline/bindings/fish"
+source /usr/share/powerline/bindings/fish/powerline-setup.fish
+powerline-setup
+EOF
