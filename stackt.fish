2cd9cac2b51d566349b01fb62c6024ba803a09ff
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-10-19 20:35:09 +0200

    set memory threshold on running tests

--- stackt.fish
+++ stackt.fish
@@ -1,4 +1,6 @@
 # Defined in - @ line 1
 function stackt --wraps='stack test' --description 'alias stackt stack test'
+  ulimit -Sv 3000000
   stack test '--test-arguments=--color' $argv 2>&1 | less -r;
+  ulimit 1>/dev/null
 end
