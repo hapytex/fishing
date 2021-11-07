c3b204600478b8fd92d65289f170b32b431b79e7
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-11-07 14:33:42 +0100

    let pointfree work with a join of the parameters

--- pointfree.fish
+++ pointfree.fish
@@ -1,4 +1,5 @@
 # Defined in - @ line 1
 function pointfree --wraps='lambdabot -e "@pl $argv"' --description 'alias pointfree lambdabot -e "@pl $argv"'
-  lambdabot -e "@pl $argv";
+  set pf (string join ' ' "$argv")
+  lambdabot -e "@pl $pf";
 end
