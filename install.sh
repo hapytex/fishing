7d8b443d17700618c887e3182102d270a98bdd64
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-07-29 17:25:33 +0200

    extra tools to work with

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get install apt-file cloc curl djinn fish git hlint hoogle lambdabot nodejs npm pandoc pdfsam powerline rename ulimit yarn
+sudo apt-get install apt-file agrep cloc curl djinn fish git hlint hoogle lambdabot nodejs npm pandoc pdfsam powerline pylint rename ulimit yarn
 
 git config --global push.default current
 
-pip3 install --upgrade black numpy pandas scipy sympy
+pip3 install --upgrade black Click numpy pandas scipy sympy
 
 # load the apt cache to find out what package exposes a certain command
 sudo apt-file update
