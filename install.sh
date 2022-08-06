e4e441fd9934d89ce66599f8afbc3e5d615ffec4
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-08-06 19:39:48 +0200

    run install.sh in a more parallel way

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get install apt-file agrep cloc curl djinn fish git hlint hoogle lambdabot nodejs npm pandoc pdfsam powerline pylint python3-pytest rename tox ulimit yarn
+sudo apt-get install -y apt-file agrep cloc curl djinn fish git hlint hoogle lambdabot nodejs npm pandoc pdfsam powerline pylint python3-pytest rename tox ulimit yarn
 
 git config --global push.default current
 
-pip3 install --upgrade black Click numpy pandas scipy sympy
+
+stack update &
+pip3 install --upgrade black Click numpy pandas scipy sympy youtube-dl &
 
 # load the apt cache to find out what package exposes a certain command
 sudo apt-file update
@@ -20,12 +22,15 @@ source /usr/share/powerline/bindings/fish/powerline-setup.fish
 powerline-setup
 EOF
 
+wait
+
 # Install Haskell support for vim
 tmp=$(tempfile)
 curl -L https://git.io/haskell-vim-now > "$tmp"
 bash "$tmp"
 rm -f "$tmp"
 
-stack update
-hoogle generate
+hoogle generate &
 stack install ascii-art-to-unicode  # install ASCII-art to Unicode converter
+
+wait
