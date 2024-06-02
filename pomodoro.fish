169ee768e21304afdfdbc9ab0bef709a1baaea42
Willem Van Onsem <yourfriends@hapytex.eu>
2024-06-02 18:24:11 +0200

    add audio to pomodoro

--- pomodoro.fish
+++ pomodoro.fish
@@ -5,8 +5,10 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
     set pausetype '\e[32m[short pause]\e[0m    \e[100D'
     for i in (seq 4)
       echo -en '\e[31m[working]\e[0m        \e[100D'
+      xdotool key XF86AudioPlay
       notify-send 'pomodoro' 'start working'
       sleep 1500
+      xdotool key XF86AudioPlay
       here_is_the_news
       if [ "$i" -gt  3 ]
         set pause 900
