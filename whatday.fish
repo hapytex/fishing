b1c2f3343f6b1ffc1cfab48043110dfbe38021ca
Willem Van Onsem <yourfriends@hapytex.eu>
2024-06-23 01:06:51 +0200

    caffeine indicator

--- whatday.fish
+++ whatday.fish
@@ -1,7 +1,12 @@
 # Defined in - @ line 1
 function whatday --description 'specifies the day with some counters'
   set dt (date +%s -d '0:00 UTC')
+  set hr (date +%H%M)
+  set caf '🚫☕'
+  if test \( '0800' -lt "$hr" \) -a \( "$hr" -lt '1900' \)
+    set caf '☕'
+  end
   set ds (math --scale 0 "$dt/86400")
   set dow (math "($ds % 7) + 1")
-  echo -e "\e[3""$dow""m"(date)"\e[0m"
+  echo -e "\e[4""$dow""m"(date)"\e[0m $caf"
 end
