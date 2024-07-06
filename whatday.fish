a94cd96b9ee90ae495ee0ea90f8900a1f1c52f31
Willem Van Onsem <yourfriends@hapytex.eu>
2024-07-06 13:31:13 +0200

    rainbow colors for whatday

--- whatday.fish
+++ whatday.fish
@@ -2,11 +2,12 @@
 function whatday --description 'specifies the day with some counters'
   set dt (date +%s -d '0:00 UTC')
   set hr (date +%H%M)
+  set colors '232;20;22' '255;165;0' '250;235;54' '121;195;20' '72;125;231' '75;54;157' '112;54;157'
   set caf '🚫☕'
   if test \( '0800' -lt "$hr" \) -a \( "$hr" -lt '1900' \)
     set caf '☕'
   end
   set ds (math --scale 0 "$dt/86400")
-  set dow (math "($ds % 7) + 1")
-  echo -e "\e[4""$dow""m"(date)"\e[0m $caf"
+  set dow (math "($ds + 4) % 7")
+  echo -e "\e[48;2;""$colors[$dow]""m"(date)" $caf\e[0m"
 end
