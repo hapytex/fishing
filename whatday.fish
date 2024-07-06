2b83b212bf978c1ac9b94f8818acee98dd98d84c
Willem Van Onsem <yourfriends@hapytex.eu>
2024-07-06 13:34:37 +0200

    day polarity

--- whatday.fish
+++ whatday.fish
@@ -3,11 +3,13 @@ function whatday --description 'specifies the day with some counters'
   set dt (date +%s -d '0:00 UTC')
   set hr (date +%H%M)
   set colors '232;20;22' '255;165;0' '250;235;54' '121;195;20' '72;125;231' '75;54;157' '112;54;157'
+  set polars '←' '→'
   set caf '🚫☕'
   if test \( '0800' -lt "$hr" \) -a \( "$hr" -lt '1900' \)
     set caf '☕'
   end
   set ds (math --scale 0 "$dt/86400")
-  set dow (math "($ds + 4) % 7")
-  echo -e "\e[48;2;""$colors[$dow]""m"(date)" $caf\e[0m"
+  set dow (math "(($ds + 4) % 7) + 1")
+  set dp (math "($ds % 2) + 1")
+  echo -e "\e[48;2;$colors[$dow]m$polars[$dp]  "(date)" $caf\e[0m"
 end
