f8457b6f320acdddea50cd2e667b378fa32877e9
Willem Van Onsem <yourfriends@hapytex.eu>
2024-07-06 22:41:28 +0200

    better cursor bookkeeping

--- pomodoro.fish
+++ pomodoro.fish
@@ -3,7 +3,7 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
   set banners (gsettings get org.gnome.desktop.notifications show-banners)
   while true
     set pause 300
-    set pausetype '\e[8D\e[32m[short pause]\e[0m '
+    set pausetype '\e[10D\e[32m[short pause]\e[0m '
     for i in (seq 4)
       echo -en '\e[100D                      \e[100D\e[31m[working]\e[0m '
       xdotool key XF86AudioPlay
@@ -14,7 +14,7 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
       here_is_the_news
       if [ "$i" -gt  3 ]
         set pause 900
-        set pausetype '\e[8D\e[33m[long pause]\e[0m '
+        set pausetype '\e[10D\e[33m[long pause]\e[0m '
       end
       echo -en "$pausetype"
       gsettings set org.gnome.desktop.notifications show-banners "$banners"
