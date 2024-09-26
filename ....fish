5d47b204169f4f7bee280d883babc7baebca1ce4
Willem Van Onsem <yourfriends@hapytex.eu>
2024-09-26 16:00:51 -0400

    fix ... command

--- ....fish
+++ ....fish
@@ -1,3 +1,10 @@
-function ... --description 'move to the root directory of the git repository'
-  cd (git rev-parse --show-toplevel)
+function ... --description 'uses ... as an abbreviation of the git root'
+    set -l root (git rev-parse --show-toplevel)
+    or return
+
+    if set -q argv[1]
+      string replace '...' -- $root $argv
+    else
+      cd "$root"
+    end
 end
