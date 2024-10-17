bb42e62f84bc35fcff538f5127f802002a4ad95c
Willem Van Onsem <yourfriends@hapytex.eu>
2024-10-18 00:40:40 +0200

    rmake

---
+++ rmake.fish
@@ -0,0 +1,10 @@
+function rmake --wraps='git pull' --description 'walk up the filetree until it finds a Makefile'
+  set cur (pwd)
+  set pre ''
+  while [ ! -f 'Makefile' ]
+    set pre (basename (pwd))"/$pre"
+    cd '..'
+  end
+  make "$pre"$argv
+  cd "$cur"
+end
