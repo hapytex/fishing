bfe73a4a26e86668d764e5675e9af03621e2a5d4
Willem Van Onsem <yourfriends@hapytex.eu>
2024-10-30 18:57:44 +0100

    fetching media

--- assets/music/fetch_item.sh
+++ assets/music/fetch_item.sh
@@ -5,6 +5,6 @@ while read p; do
   out=$(cut -d ':' -f 1 <<<"$p")
   echo "$url/$out"
   yt-dlp -f 251 "$url" -o - > temp.webm
-  ffmpeg -i temp.webm -vn -ab 128k -ar 44100 -s "$mn" -t "$mx" -y "$out.mp3"
+  ffmpeg -i temp.webm -vn -ab 128k -ar 44100 -y "$out.mp3" </dev/null
 done
 rm temp.webm
