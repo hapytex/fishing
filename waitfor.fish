5b1fa76f5138a4ced7dfc9973bfadf2b3822abef
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-03 22:45:18 +0100

    set title for pomodoro

--- waitfor.fish
+++ waitfor.fish
@@ -1,6 +1,9 @@
-function waitfor --description 'wait a certain amount of time, or until the user hits ENTER' -a time -a label
+function waitfor --description 'wait a certain amount of time, or until the user hits ENTER' -a time -a label -a desc
   if [ ! -z "$label" ]
-    echo -en "$label "
+    set label "$label "
+    echo -en "$label"
+  else
+    set label ''
   end
   for i in (seq "$time" -1 0)
     set mn (math "$i % 60")
@@ -8,6 +11,7 @@ function waitfor --description 'wait a certain amount of time, or until the user
     set txt (printf "%02d:%02d" $hr $mn)
     set n (string length "$txt")
     echo -en "$txt\e["$n"D"
+    echo -en "\033]0;$desc$txt\007"
     sleep 1
   end
 end
