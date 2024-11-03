a728482d086984de59ef8f8cc16e2f47c2d4b353
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-03 02:09:20 +0100

    chomp

--- pomodoro.fish
+++ pomodoro.fish
@@ -2,7 +2,7 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
   set banners (gsettings get org.gnome.desktop.notifications show-banners)
   while true
     set pause 300
-    set pausetype '\e[10D\e[32m[short pause]\e[0m '
+    set pausetype '\e[10D\e[32m[short pause]\e[0m'
     for i in (seq 4)
       xdotool key XF86AudioPlay
       notify-send -i /usr/share/icons/hicolor/64x64/apps/io.github.alarm-clock-applet.clock.png -c productivity -u low 'pomodoro ⏰' 'start working'
@@ -12,7 +12,7 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
       here_is_the_news
       if [ "$i" -gt  3 ]
         set pause 900
-        set pausetype '\e[10D\e[33m[long pause]\e[0m '
+        set pausetype '\e[10D\e[33m[long pause]\e[0m'
       end
       gsettings set org.gnome.desktop.notifications show-banners "$banners"
       notify-send -i /usr/share/icons/hicolor/64x64/apps/io.github.alarm-clock-applet.clock.png -c productivity -u low 'pomodoro ⏰' 'take a break'
