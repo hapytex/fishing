a337f9648b13ee3cacfb9ad622ad293f3db5539d
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-04 18:15:00 +0100

    retag mp3 files

--- assets/music/fetch_item.sh
+++ assets/music/fetch_item.sh
@@ -14,7 +14,7 @@ while [ "$#" -gt 0 ]; do
     yt-dlp -f 251 "$url" -o - > temp.webm
     ffmpeg -i temp.webm -vn -ab 128k -ar 44100 -y "$out.mp3" </dev/null
     nn=$(sed 's/[_ -]+/ /g' <<<"$out")
-    id3v2 -t "$nn" -g 31 -T "$i/$n" -A "$album" -a "$album" "$f" -y "$yy"
+    id3v2 -t "$nn" -g 31 -T "$i/$n" -A "$album" -a "$album" -y "$yy" "$out.mp3"
     i=$((i+1))
   done < "$nm"
   shift
