c7b4dcbc2efafc3361b886e933de8308a09f6fa3
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-14 10:37:19 +0100

    use ffplay for here_is_the_news

--- here_is_the_news.fish
+++ here_is_the_news.fish
@@ -1,7 +1,10 @@
 function here_is_the_news --description 'beeps three times to notify an event'
-  play -n synth 0.1 sine 880 vol 0.5 2>/dev/null
+  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=800:d=0.1' >/dev/null 2>/dev/null
+  # play -n synth 0.1 sine 880 vol 0.5 2>/dev/null
   sleep 0.9
-  play -n synth 0.1 sine 880 vol 0.5 2>/dev/null
+  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=800:d=0.1' >/dev/null 2>/dev/null
+  # play -n synth 0.1 sine 880 vol 0.5 2>/dev/null
   sleep 0.9
-  play -n synth 1 sine 880 vol 0.5 2>/dev/null
+  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=800:d=1' >/dev/null 2>/dev/null
+  # play -n synth 1 sine 880 vol 0.5 2>/dev/null
 end
