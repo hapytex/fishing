9520cea58f1f6ddd75e1d62bc0d551d0a10907de
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-14 20:11:43 +0100

    https://en.wikipedia.org/wiki/Greenwich_Time_Signal

--- here_is_the_news.fish
+++ here_is_the_news.fish
@@ -1,7 +1,8 @@
 function here_is_the_news --description 'beeps three times to notify an event'
-  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null &
-  sleep 1
-  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null &
-  sleep 1
+  # https://en.wikipedia.org/wiki/Greenwich_Time_Signal
+  for __ in (seq 5)
+    ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null &
+    sleep 1
+  end
   ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.5' >/dev/null 2>/dev/null
 end
