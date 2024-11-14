baed946f5ac10d03e0efd8024e5c85760a5b0411
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-14 20:55:39 +0100

    configurable prebeeps

--- here_is_the_news.fish
+++ here_is_the_news.fish
@@ -1,6 +1,7 @@
-function here_is_the_news --description 'beeps three times to notify an event'
+function here_is_the_news --description 'beeps three times to notify an event' -a n
   # https://en.wikipedia.org/wiki/Greenwich_Time_Signal
-  for __ in (seq 5)
+  set -q argv[1] || set n 5
+  for __ in (seq "$n")
     ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null &
     sleep 1
   end
