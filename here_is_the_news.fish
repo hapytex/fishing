67124665e408d34fe225a143c6beef39094ed924
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-14 10:54:09 +0100

    no exit wait

--- here_is_the_news.fish
+++ here_is_the_news.fish
@@ -1,7 +1,7 @@
 function here_is_the_news --description 'beeps three times to notify an event'
-  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null
-  sleep 0.9
-  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null
-  sleep 0.9
+  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null &
+  sleep 1
+  ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.1' >/dev/null 2>/dev/null &
+  sleep 1
   ffplay -autoexit -nodisp -f lavfi -i 'sine=f=1000:d=0.5' >/dev/null 2>/dev/null
 end
