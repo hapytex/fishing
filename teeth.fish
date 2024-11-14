8a1b2d4c3106a5766e302c959e1194ba9a9f81ce
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-14 22:06:23 +0100

    add tongue

--- teeth.fish
+++ teeth.fish
@@ -1,11 +1,12 @@
 function teeth --description 'help cleaning teeth'
   function draw_teeth -a cc -a tt -a kin
-    set teeth_map '   o5▁ o4▁ o4▁ o3▁ o3▁ o2▁    \n   u5█ u4█ u4█ u3█ u3█ u2█    \n   i5▔ i4▔ i4▔ i3▔ i3▔ i2▔    \no5▕u5█i5▏           i2▕u2█o2▏    \no6▕u6█i6▏           i1▕u1█o1▏\no6▕u6█i6▏           i1▕u1█o1▏\no7▕u7█i7▏           i0▕u0█o0▏\no7▕u7█i7▏           i0▕u0█o0▏' 'o7▕u7█i7▏           i0▕u0█o0▏\no7▕u7█i7▏           i0▕u0█o0▏\no6▕u6█i6▏           i1▕u1█o1▏\no6▕u6█i6▏           i1▕u1█o1▏\no5▕u5█i5▏           i2▕u2█o2▏\n   i5▁ i4▁ i4▁ i3▁ i3▁ i2▁     \n   u5█ u4█ u4█ u3█ u3█ u2█    \n   o5▔ o4▔ o4▔ o3▔ o3▔ o2▔    '
+    set teeth_map 'o7▕u7█i7▏   T████    i0▕u0█o0▏\no7▕u7█i7▏   T████    i0▕u0█o0▏\no6▕u6█i6▏   T████    i1▕u1█o1▏\no6▕u6█i6▏   T▜██▛    i1▕u1█o1▏\no5▕u5█i5▏           i2▕u2█o2▏\n   i5▁ i4▁ i4▁ i3▁ i3▁ i2▁    \n   u5█ u4█ u4█ u3█ u3█ u2█    \n   o5▔ o4▔ o4▔ o3▔ o3▔ o2▔    ' '   o5▁ o4▁ o4▁ o3▁ o3▁ o2▁\n   u5█ u4█ u4█ u3█ u3█ u2█\n   i5▔ i4▔ i4▔ i3▔ i3▔ i2▔\no5▕u5█i5▏           i2▕u2█o2▏\no6▕u6█i6▏           i1▕u1█o1▏\no6▕u6█i6▏           i1▕u1█o1▏\no7▕u7█i7▏           i0▕u0█o0▏\no7▕u7█i7▏           i0▕u0█o0▏'
     set tooth_map $teeth_map[$cc]
     set col (echo -e '\e[97m')
+    set tongue (echo -e '\e[31m')
     set ecol (echo -e '\e[90m')
     # echo "$kin -- $tt"
-    echo -e (echo "\e[90m$tooth_map\e[0m" | sed -E "s/$kin$tt(.)/$col\1$ecol/g" | sed -E 's/[oui][0-9](.)/\1/g')
+    echo -e (echo "\e[90m$tooth_map\e[0m" | sed -E "s/$kin$tt(.)/$col\1$ecol/g;s/[oui][0-9](.)/\1/g;s/T([^ ]*)/$tongue\1$ecol/g")
   end
   # based on the Tooth brushing timer of the Delta Dental of Tennessee https://youtu.be/gAODutgIIVQ
   set cheeks ⬆️  ⬇️
