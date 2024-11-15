cefe2e265b963111c60495eb66d5dcb7b572f30d
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-16 00:07:15 +0100

    recd

--- sleep_for.fish
+++ sleep_for.fish
@@ -2,6 +2,8 @@ function sleep_for --description 'Sleep a given number of hours' -a n
   if [ -z "$n" ]
     set n 7
   end
+  set pth (/usr/bin/pwd)
   cd "$fish_function_path[1]/assets/music/"
   nohup nice -n 19 vlc --no-random --no-loop --qt-start-minimized --play-and-exit $n'h_sleep.xspf' >/dev/null 2>/dev/null &
+  cd "$pth"
 end
