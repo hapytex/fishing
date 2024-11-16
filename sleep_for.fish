12f32c8007ab01ea0d61980281e8ead3fc31e788
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-16 23:28:50 +0100

    disown

--- sleep_for.fish
+++ sleep_for.fish
@@ -5,5 +5,6 @@ function sleep_for --description 'Sleep a given number of hours' -a n
   set pth (/usr/bin/pwd)
   cd "$fish_function_path[1]/assets/music/"
   nohup nice -n 19 vlc --no-random --no-loop --qt-start-minimized --play-and-exit $n'h_sleep.xspf' >/dev/null 2>/dev/null &
+  disown "$last_pid"
   cd "$pth"
 end
