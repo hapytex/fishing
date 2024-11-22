814a7838c7e164ee10397d5343bef6bf2e897a6c
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-22 18:32:18 +0100

    default black

--- fill.fish
+++ fill.fish
@@ -1,7 +1,7 @@
 function fill --description 'fill the entire screen with a certain color' -a r -a g -a b
-  test -z r || set r 0
-  test -z g || set g 0
-  test -z b || set b 0
+  test -n "$r" || set r 0
+  test -n "$g" || set g 0
+  test -n "$b" || set b 0
   set l (printf %(tput cols)s)
   for i in (seq (tput lines))
     echo -en "\e[48;2;$r;$g;"$b"m$l\e[0m"
