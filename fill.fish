5c1194106fb7cba5ebc6c3930635d07874c14c3f
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-22 18:28:38 +0100

    parameterize color

--- fill.fish
+++ fill.fish
@@ -1,6 +1,6 @@
-function fill --description 'fill the entire screen with a certain color'
-  set r (printf %(tput cols)s)
+function fill --description 'fill the entire screen with a certain color' -a r -a g -a b
+  set l (printf %(tput cols)s)
   for i in (seq (tput lines))
-    echo -en "\e[48;2;0;184;252m$r\e[0m"
+    echo -en "\e[48;2;$r;$g;"$b"m$l\e[0m"
   end
 end
