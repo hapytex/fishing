aac1b4544ca42c4e81b0113cf4e93caa651f9b8a
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-23 10:14:21 +0100

    focus/relax light

--- sleep_for.fish
+++ sleep_for.fish
@@ -1,7 +1,6 @@
 function sleep_for --description 'Sleep a given number of hours' -a n
-  if [ -z "$n" ]
-    set n 7
-  end
+  test -n "$n" || set n 7
+  fill 0 126 61
   set pth (/usr/bin/pwd)
   cd "$fish_function_path[1]/assets/music/"
   nohup nice -n 19 vlc --no-random --no-loop --qt-start-minimized --play-and-exit $n'h_sleep.xspf' >/dev/null 2>/dev/null &
