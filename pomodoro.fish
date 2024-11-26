196cc4b901892141cdd25d58116f7debc2223fc3
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-27 00:12:09 +0100

    random noise

--- pomodoro.fish
+++ pomodoro.fish
@@ -1,8 +1,8 @@
 function pomodoro --description 'the pomodoro technique to stay focussed'
   fill 0 184 252
   set banners (gsettings get org.gnome.desktop.notifications show-banners)
-  function noising -a i
-    switch (math "$i % 3")
+  function noising
+    switch (random 0 2)
       case 0
         timeout 1800 play -q -n synth brownnoise >/dev/null 2>/dev/null &
       case 1
@@ -12,7 +12,6 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
     end
     echo "$last_pid"
   end
-  set ni 0
   while true
     set pause 300
     set pausetype '\e[10D\e[32m[short pause]\e[0m'
@@ -20,8 +19,7 @@ function pomodoro --description 'the pomodoro technique to stay focussed'
       xdotool key XF86AudioPlay
       notify-send -i /usr/share/icons/hicolor/64x64/apps/io.github.alarm-clock-applet.clock.png -c productivity -u low 'pomodoro ⏰' '▶️ start working'
       gsettings set org.gnome.desktop.notifications show-banners false
-      set gamma (noising "$ni")
-      set ni (math "$ni + 1")
+      set gamma (noising)
       waitfor 1500 '\e[100D                      \e[100D\e[31m[working]\e[0m' '▶️  working '
       kill "$gamma"
       xdotool key XF86AudioPlay
