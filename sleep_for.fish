6bd8f5c58ca48a062d21fc24959c4675e6b667de
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-28 20:10:52 +0100

    pink noise sleep

--- sleep_for.fish
+++ sleep_for.fish
@@ -1,12 +1,19 @@
 function sleep_for --description 'Sleep a given number of hours' -a n
   test -n "$n" || set n 7
-  set s (math "3600*$n")
   fill 0 126 61
-  set pth (/usr/bin/pwd)
-  cd "$fish_function_path[1]/assets/music/"
-  nohup nice -n 19 vlc --no-random --no-loop --qt-start-minimized --play-and-exit $n'h_sleep.xspf' >/dev/null 2>/dev/null &
-  disown "$last_pid"
-  cd "$pth"
-  nohup fish -c "sleep $s; fill 255 219 100 | tee "(tty) >/dev/null 2>/dev/null &
+  keycolor 0 0 0 0
+  if [ "$n" -gt 0 ]
+    set pth (/usr/bin/pwd)
+    cd "$fish_function_path[1]/assets/music/"
+    nohup nice -n 19 vlc --no-random --no-loop --qt-start-minimized --play-and-exit $n'h_sleep.xspf' >/dev/null 2>/dev/null &
+    disown "$last_pid"
+    cd "$pth"
+  else
+    set n (math "-$n")
+    timeout (math "3600*$n") play -q -n synth pinknoise >/dev/null 2>/dev/null &
+    disown "$last_pid"
+  end
+  set s (math "3600*$n")
+  nohup fish -c "sleep $s; keycolor 255 219 100 255; fill 255 219 100 | tee "(tty) >/dev/null 2>/dev/null &
   disown "$last_pid"
 end
