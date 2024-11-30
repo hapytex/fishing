1a772171233d45bae83436cb7d62eb3092c5c6af
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-30 16:55:37 +0100

    advent candle

---
+++ advent.fish
@@ -0,0 +1,12 @@
+function advent --description 'Light the candles for advent'  -a day
+  set candles 🕯️ 🕯️🕯️ 🕯️🕯️🕯️ 🕯️🕯️🕯️🕯️
+  set christmas (date +%s --utc -d 'december 25 0:00')
+  set weekday (date +%w -d "@$christmas")
+  test $weekday -eq 0 && set weekday 7
+  set last (math "$christmas - ($weekday * 86400)")
+  set cur (date +%s --utc -d "$day")
+  set cand (math "4 - ceil(($last - $cur) / 604800)")
+  if [ "$cand" -le 4 -a "$cand" -gt 0 ]
+    echo $candles[$cand]
+  end
+end
