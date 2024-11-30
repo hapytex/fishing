bb04a9814e7cf301580c99625ff21461996c2318
Willem Van Onsem <yourfriends@hapytex.eu>
2024-11-30 09:44:57 +0100

    ampels

--- ampel.fish
+++ ampel.fish
@@ -1,4 +1,11 @@
 function ampel --description 'Get the ampel color for the given index' -a i
   set ampels 🟢 🔶 🟥
-  echo $ampels[(math "$i+1")]
+  if [ "$i" -ge 2 ]
+    set i 3
+  else if [ "$i" -le 0 ]
+    set i 1
+  else
+    set i (math "$i+1")
+  end
+  echo $ampels[$i]
 end
