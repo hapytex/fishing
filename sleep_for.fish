c7c30dcf3ef3d8dd76e559fa43119b11b6f274d7
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-01 00:11:22 +0100

    simplify assets

--- sleep_for.fish
+++ sleep_for.fish
@@ -4,7 +4,7 @@ function sleep_for --description 'Sleep a given number of hours' -a n
   keycolor 100 75 0 25
   if [ "$n" -gt 0 ]
     set pth (/usr/bin/pwd)
-    cd "$fish_function_path[1]/assets/music/"
+    cd (assets)'/music/'
     nohup nice -n 19 vlc --no-random --no-loop --qt-start-minimized --play-and-exit $n'h_sleep.xspf' >/dev/null 2>/dev/null &
     disown "$last_pid"
     cd "$pth"
