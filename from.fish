2e80c3d9a32cf3afa6ed3afa05ff8d8bef269bc4
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-01 22:02:03 +0100

    Python terminal title

--- from.fish
+++ from.fish
@@ -1,4 +1,5 @@
 function from --wraps='/usr/bin/ipython3' --wraps='/usr/bin/ipython3'
+  echo -en '\033]0;🐍 Python shell\7' &
   set tmpfile (mktemp '/tmp/XXXXXXXXXXXX.py')
   echo "from $argv" > "$tmpfile"
   /usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx -i "$tmpfile"; 
--- python3.fish
+++ python3.fish
@@ -1,3 +1,4 @@
 function python3 --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit -c "import numpy as np"' --description 'alias python3=/usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx'
+  echo -en '\033]0;🐍 Python shell\7' &
   /usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx $argv; 
 end
