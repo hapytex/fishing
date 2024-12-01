354573dcebb567fa50b60a528ed57f8ecda50241
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-01 21:57:49 +0100

    denoising functions

--- python3.fish
+++ python3.fish
@@ -1,3 +1,3 @@
-function python3 --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit -c "import numpy as np"' --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit' --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib' --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=qt' --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx' --description 'alias python3=/usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx'
+function python3 --wraps='/usr/bin/ipython3 --no-banner --no-confirm-exit -c "import numpy as np"' --description 'alias python3=/usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx'
   /usr/bin/ipython3 --no-banner --no-confirm-exit --matplotlib=wx $argv; 
 end
