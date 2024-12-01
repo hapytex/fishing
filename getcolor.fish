86d6eb1180800735373418e52b230c25760c9f85
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-01 12:00:52 +0100

    config palette

--- getcolor.fish
+++ getcolor.fish
@@ -1,3 +1,4 @@
-function getcolor --description 'get the color for a given name' -a name
-  jq "."(string replace -r '\s+' '_' $name | tr '[:upper:]' '[:lower:]') (assets)/colors.json
+function getcolor --description 'get the color for a given name' -a name -a palette
+  test -n "$palette" || set palette 'colors'
+  jq "."(string replace -r '\s+' '_' $name | tr '[:upper:]' '[:lower:]') (assets)"/$palette.json"
 end
