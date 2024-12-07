6b6501761034b22fbfd413aac8cea4a778c12f30
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-08 00:18:04 +0100

    gradual wakeup in 1500 secs

--- wakeup.fish
+++ wakeup.fish
@@ -1,8 +1,9 @@
 function wakeup --description 'Make some noise to wake someone up' -a tty
   xset dpms force on
   for i in (seq 0 4)
+    xrandr --output eDP-1 --brightness (math "($i+1)/5")
     set cols (getcolor $i wakeup_colors)
-    keycolor $cols 255
+    keycolor $cols (math "50*$i+55")
     fill $cols > "$tty"
     sleep 300
   end
