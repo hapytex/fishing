62c67b85a9e4ef2a1481403a5fda1d17caddca9c
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-08 10:32:24 +0100

    specify terminal size

--- sleep_for.fish
+++ sleep_for.fish
@@ -16,6 +16,6 @@ function sleep_for --description 'Sleep a given number of hours' -a n
   xrandr --output eDP-1 --brightness '0.25'
   xset dpms force off
   set s (math "3600*$n - 1500")
-  nohup fish -c "sleep $s; wakeup "(tty) 2>/dev/null &
+  nohup fish -c "sleep $s; wakeup "(tty)' '(tput lines)' '(tput cols) 2>/dev/null &
   disown "$last_pid"
 end
--- wakeup.fish
+++ wakeup.fish
@@ -1,10 +1,10 @@
-function wakeup --description 'Make some noise to wake someone up' -a tty
+function wakeup --description 'Make some noise to wake someone up' -a tty -a l -a c
   xset dpms force on
   for i in (seq 0 4)
     xrandr --output eDP-1 --brightness (math "($i+1)/5")
     set cols (getcolor $i wakeup_colors)
     keycolor $cols (math "50*$i+55")
-    fill $cols > "$tty"
+    fill $cols $l $c > "$tty"
     sleep 300
   end
 end
