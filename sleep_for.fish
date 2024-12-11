c750848fd631f85b8e706a589c2c91cf0fb787f8
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-11 22:05:06 +0100

    make sleep eager

--- sleep_for.fish
+++ sleep_for.fish
@@ -11,12 +11,11 @@ function sleep_for --description 'Sleep a given number of hours' -a n
     cd "$pth"
   else
     set n (math "-$n")
-    timeout (math "3600*$n") play -q -n synth pinknoise >/dev/null 2>/dev/null &
-    disown "$last_pid"
+    timeout (math "3600*$n") play -q -n synth pinknoise
   end
   xrandr --output eDP-1 --brightness '0.25'
   xset dpms force off
   set s (math "3600*$n - 1500")
-  nohup fish -c "sleep $s; wakeup "(tty)' '(tput lines)' '(tput cols) 2>/dev/null &
-  disown "$last_pid"
+  sleep "$s"
+  wakeup "(tty)' '(tput lines)' '(tput cols)
 end
