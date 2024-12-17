db734464945c348589a84a84b829296fc7e23ce9
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-17 23:12:38 +0100

    369

--- wakeup.fish
+++ wakeup.fish
@@ -3,9 +3,9 @@ function wakeup --description 'Make some noise to wake someone up' -a tty -a l -
   xset dpms force on
   switch (random 0 1)
     case 0
-      play -q -n synth 1500 sine 220 sine 221/260 fade 15 vol 0.2 &
+      play -q -n synth 1500 sine 369 sine 370/409 fade 15 vol 0.2 &
     case 1
-      play -q -n synth 1500 sine 221/260 sine 220 fade 15 vol 0.2 &
+      play -q -n synth 1500 sine 370/409 sine 369 fade 15 vol 0.2 &
   end
   for i in (seq 0 4)
     xrandr --output eDP-1 --brightness (math "($i+1)/5")
