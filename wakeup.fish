266bce6883bd0c7eeab73250c2381616e85dc1f4
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-17 19:19:58 +0100

    balance freqs

--- wakeup.fish
+++ wakeup.fish
@@ -1,6 +1,11 @@
 function wakeup --description 'Make some noise to wake someone up' -a tty -a l -a c
   xset dpms force on
-  play -q -n synth 1500 sine 100 sine 101/140 &
+  switch (random 0 1)
+    case 0
+      play -q -n synth 1500 sine 100 sine 101/140 &
+    case 1
+      play -q -n synth 1500 sine 101/140 sine 100 &
+  end
   for i in (seq 0 4)
     xrandr --output eDP-1 --brightness (math "($i+1)/5")
     set cols (getcolor $i wakeup_colors)
