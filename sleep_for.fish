f825ec3eea215db7919ce0c0fca10cec3da996d5
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-20 18:09:21 +0100

    prevent suspending the laptop

--- sleep_for.fish
+++ sleep_for.fish
@@ -16,6 +16,8 @@ function sleep_for --description 'Sleep a given number of hours' -a n
     set n (math "-$n")
     timeout (math "3600*$n") play -q -n synth pinknoise vol 0.0125 &
   end
+  set nn (math "$n+1")
+  date '+%Y-%m-%d %H:%M:%S' -d "+$nn hours" >> "$HOME/block_sleep"
   xrandr --output eDP-1 --brightness '0.25'
   xset dpms force off
   set eps (math "6*$n-3")
