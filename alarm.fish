093f436ff90a2bfb1fe7c3d9830f3a0933a66f81
Willem Van Onsem <yourfriends@hapytex.eu>
2024-12-24 07:24:57 +0100

    alarm trap

--- alarm.fish
+++ alarm.fish
@@ -1,5 +1,10 @@
 function alarm --description 'show distress'
+  function endpom -a gamma
+    test -n "$gamma" && kill "$gamma" >/dev/null || true
+  end
   timeout 3600 play -q -n synth 120 sine 500 sine 540 vol 0.5 &
+  set gamma $last_pid
+  trap "endpom $gamma" EXIT
   while true
     for i in (seq 55 50 255)
       keycolor 237 17 6 "$i" &
