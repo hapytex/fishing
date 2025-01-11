2510de19e9d229ab7517ce0a8b5f96a482b2f7db
Willem Van Onsem <yourfriends@hapytex.eu>
2025-01-11 18:36:39 +0100

    timestamp with utc

--- gh_status.fish
+++ gh_status.fish
@@ -1,5 +1,5 @@
 function gh_status --description 'set the GitHub status' -a message -a emoji -a exp -a limit
-  test -n "$exp" && set exp (date '+%Y-%m-%d %H:%M:%S%z' -d "$exp")
+  test -n "$exp" && set exp (date --utc '+%Y-%m-%dT%H:%M:%SZ' -d "$exp")
   gh api graphql --raw-field emoji=":$emoji:" --raw-field message="$message" --raw-field expires="$exp" --field limit=$limit --raw-field query='
     mutation UpdateStatus($emoji: String!, $message: String!, $expires: DateTime, $limit: Boolean!) {
       changeUserStatus(input:{emoji: $emoji, message: $message, expiresAt: $expires, limitedAvailability: $limit}) {
@@ -14,5 +14,5 @@ function gh_status --description 'set the GitHub status' -a message -a emoji -a 
           }
         }
       }
-    }' >/dev/null 2>/dev/null &
+    }' # >/dev/null 2>/dev/null &
 end
