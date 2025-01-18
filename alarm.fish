1ba9cdf73c2a558ad4794ecd7ed7fd639d759b51
Willem Van Onsem <yourfriends@hapytex.eu>
2025-01-18 09:29:41 +0100

    raise the alarm

--- alarm.fish
+++ alarm.fish
@@ -2,16 +2,26 @@ function alarm --description 'show distress'
   function endpom -a gamma
     test -n "$gamma" && kill "$gamma" >/dev/null || true
   end
+  xrandr --output eDP-1 --brightness '1.0' &
+  xset dpms force on &
   timeout 3600 play -q -n synth 120 sine 500 sine 540 vol 0.5 &
   set gamma $last_pid
   trap "endpom $gamma" EXIT
   while true
     for i in (seq 55 50 255)
+      set rr (math "round(237*$i/255)")
+      set gg (math "round(17*$i/255)")
+      set bb (math "round(6*$i/255)")
       keycolor 237 17 6 "$i" &
+      fill $rr $gg $bb
       sleep 0.2
     end
     for i in (seq 200 -50 5)
+      set rr (math "round(237*$i/255)")
+      set gg (math "round(17*$i/255)")
+      set bb (math "round(6*$i/255)")
       keycolor 237 17 6 "$i" &
+      fill $rr $gg $bb
       sleep 0.2
     end
   end
--- wakeup.fish
+++ wakeup.fish
@@ -1,7 +1,5 @@
 function wakeup --description 'Make some noise to wake someone up' -a tty -a l -a c
   test -n "$tty" | set tty (tty)
-  xrandr --output eDP-1 --brightness 0.2
-  xset dpms force on
   switch (random 0 1)
     case 0
       play -q -n synth 1500 sine 369 sine 370/409 fade 750 vol 0.2 &
@@ -9,7 +7,8 @@ function wakeup --description 'Make some noise to wake someone up' -a tty -a l -
       play -q -n synth 1500 sine 370/409 sine 369 fade 750 vol 0.2 &
   end
   for i in (seq 0 4)
-    xrandr --output eDP-1 --brightness (math "($i+1)/5")
+    xrandr --output eDP-1 --brightness (math "($i+1)/5") &
+    xset dpms force on &
     set cols (getcolor $i wakeup_colors)
     keycolor $cols (math "50*$i+55")
     fill $cols $l $c > "$tty"
