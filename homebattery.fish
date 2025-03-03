251ed00e12e788499a4a86817ab7ffdb2622c911
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-03 07:20:19 +0100

    home battery

---
+++ homebattery.fish
@@ -0,0 +1,10 @@
+function homebattery --description 'determine how long it will take to load the battery full' -a cur -a upto
+  test -n "$cur" || set cur 0
+  test -n "$upto" || set upto 100
+  # 0% -> 80% @300Watts
+  set s0 (math "min(80, $upto) - min(80, $cur)")
+  # 80% -> 100% @150Watts
+  set s1 (math "max(80, $upto) - max(80, $cur)")
+  set tot (math "round(73728*$s0/300 + 73728*$s1/150)")
+  waitfor "$tot" 'battery' 'bat '
+end
