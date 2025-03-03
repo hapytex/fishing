3ded88135aa5c3bb04209a7e984173c7158dfe74
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-03 07:59:50 +0100

    fix washingmachine

--- washingmachine.fish
+++ washingmachine.fish
@@ -1,4 +1,4 @@
 function washingmachine --description 'wait until the washing machine has finished'
-  echo -e 'takes \e[1m53%\[0m of the home battery'
+  echo -e 'takes \e[1m53%\e[0m of the home battery'
   waitfor 4380 '👕 washing machine' '👕 '
 end
