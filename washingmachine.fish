7d6660d3675749b3ad6074e55ecf6c42dd877c14
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-06 21:39:58 +0100

    water usage

--- washingmachine.fish
+++ washingmachine.fish
@@ -1,4 +1,4 @@
 function washingmachine --description 'wait until the washing machine has finished'
-  echo -e 'takes \e[1m53%\e[0m of the home battery'
+  echo -e 'takes \e[1m53%\e[0m of the home battery and \e[1m70ℓ\e[0m water'
   waitfor 4380 '👕 washing machine' '👕 '
 end
