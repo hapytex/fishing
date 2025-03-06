05ff25c3491323e0167e28d0908b5305a1ab8d94
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-06 22:00:20 +0100

    utility function

--- dishwasher.fish
+++ dishwasher.fish
@@ -1,4 +1,3 @@
 function dishwasher --description 'wait until the dishwasher has finished'
-  echo -e 'takes \e[1m48%\e[0m of the home battery and \e[1m10ℓ\e[0m water'
-  waitfor 11700 '🍽️  dishwasher' '🍽️ '
+  household 11700 '🍽️' 'dishwasher' 48 10
 end
