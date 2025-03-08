6006d65fc9d37251402a036340457d3f8d6e8e61
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-08 09:51:52 +0100

    prevent coffee outside coffee hours

--- coffee.fish
+++ coffee.fish
@@ -1,8 +1,9 @@
 function coffee --description 'wait until the coffee machine has finished two cups of coffee'
+  set hr (date +%H%M)
   if test \( (setvar COFFEE_MIN) -le "$hr" \) -a \( "$hr" -le (setvar COFFEE_MAX) \)
     household 270 '☕☕' 'coffee' 3 0.5
   else
     echo >&2 'no coffee anymore'
-    exit 1
+    return 1
   end
 end
