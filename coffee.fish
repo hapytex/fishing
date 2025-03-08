d9af6413f716aed7ae58553b537a031e5d5a07e0
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-08 09:35:30 +0100

    check coffee times

--- coffee.fish
+++ coffee.fish
@@ -1,3 +1,8 @@
 function coffee --description 'wait until the coffee machine has finished two cups of coffee'
-  household 270 '☕☕' 'coffee' 3 0.5
+  if test \( (setvar COFFEE_MIN) -le "$hr" \) -a \( "$hr" -le (setvar COFFEE_MAX) \)
+    household 270 '☕☕' 'coffee' 3 0.5
+  else
+    echo >&2 'no coffee anymore'
+    exit 1
+  end
 end
