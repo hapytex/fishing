a67910437d0f890f79efe5939c73bc8edb369b4d
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-23 23:25:08 +0100

    survey

--- sleep_survey.fish
+++ sleep_survey.fish
@@ -2,5 +2,5 @@ function sleep_survey --description 'A short survey about sleep quality' -a deep
   test -n "$deep" || read -P 'deep sleep> ' deep
   test -n "$total" || read -P 'total sleep> ' total
   test -n "$quality" || read -P 'deep quality> ' quality
-  measure health.sleep.deep $deep health.sleep.total $total health.sleep.quality $quality
+  measure health.sleep.deep "$deep" health.sleep.total "$total" health.sleep.quality "$quality"
 end
