01c90c0a6604302ec5e7e3bd51d26c7c571de6bc
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-23 23:27:21 +0100

    prompt color

--- sleep_survey.fish
+++ sleep_survey.fish
@@ -1,6 +1,6 @@
 function sleep_survey --description 'A short survey about sleep quality' -a deep -a total -a quality
-  test -n "$deep" || read -P 'deep sleep> ' deep
-  test -n "$total" || read -P 'total sleep> ' total
-  test -n "$quality" || read -P 'deep quality> ' quality
+  test -n "$deep" || read -P (string unescape '\e[31mdeep sleep\e[0m> ') deep
+  test -n "$total" || read -P (string unescape '\e[31mtotal sleep\e[0m> ') total
+  test -n "$quality" || read -P (string unescape '\e[31msleep quality\e[0m> ') quality
   measure health.sleep.deep "$deep" health.sleep.total "$total" health.sleep.quality "$quality"
 end
