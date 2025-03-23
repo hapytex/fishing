4223dce7a5e521ed486d89235f57e9f5d0b2ec97
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-23 16:35:20 +0100

    multiple measurements at once

--- add_measure.py
+++ add_measure.py
@@ -5,7 +5,7 @@ from ast import literal_eval
 from datetime import datetime
 import json
 
-ASSET_PATH = 'measurements.json'
+ASSET_PATH = "measurements.json"
 
 if __name__ == "__main__":
     assert (
@@ -16,8 +16,10 @@ if __name__ == "__main__":
             data = json.load(f)
     except IOError:
         data = {}
-    key = sys.argv[1]
-    val = literal_eval(sys.argv[2])
-    data.setdefault(key, {})[datetime.now().isoformat()] = val
+    dt = datetime.now().isoformat()
+    for i in range(1, len(sys.argv) - 1, 2):
+        key = sys.argv[i]
+        val = literal_eval(sys.argv[i + 1])
+        data.setdefault(key, {})[dt] = val
     with open(ASSET_PATH, "w") as f:
         json.dump(data, f, indent=4)
