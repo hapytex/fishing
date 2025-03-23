c9f9838dde08999588222c378207eb92e4cdcc78
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-23 23:24:31 +0100

    prompt values

--- sleep_survey.fish
+++ sleep_survey.fish
@@ -1,3 +1,6 @@
 function sleep_survey --description 'A short survey about sleep quality' -a deep -a total -a quality
+  test -n "$deep" || read -P 'deep sleep> ' deep
+  test -n "$total" || read -P 'total sleep> ' total
+  test -n "$quality" || read -P 'deep quality> ' quality
   measure health.sleep.deep $deep health.sleep.total $total health.sleep.quality $quality
 end
