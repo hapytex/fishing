421cc0bf6ed816ac190ecde7a4128665539a0d1c
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-26 12:48:05 +0100

    add value to key for specific date

--- add_measure.py
+++ add_measure.py
@@ -5,6 +5,7 @@ from ast import literal_eval
 from datetime import datetime
 import json
 import re
+from dateparser import parse
 
 DURATION = re.compile(r"\d{1,2}([:]\d{2})+")
 ASSET_PATH = "measurements.json"
@@ -21,6 +22,14 @@ if __name__ == "__main__":
     dt = datetime.now().isoformat()
     for i in range(1, len(sys.argv) - 1, 2):
         key = sys.argv[i]
+        try:
+            key, dt2 = key.rsplit('@', 1)
+            dt2 = parse(dt2)
+            if dt2 is not None:
+                key_dt = dt2.isoformat()
+        except ValueError:
+            # key is already fine
+            key_dt = dt
         val = sys.argv[i + 1].strip()
         if DURATION.fullmatch(val.strip()):
             d = 0
@@ -39,6 +48,6 @@ if __name__ == "__main__":
         datum = data
         for ky in key.split("."):
             datum = datum.setdefault(ky, {})
-        datum[dt] = val
+        datum[key_dt] = val
     with open(ASSET_PATH, "w") as f:
         json.dump(data, f, indent=4)
