ed4e46e0210cb20b52f02a69b96ed16d118eb38e
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-29 17:42:31 +0100

    print all

--- add_list.py
+++ add_list.py
@@ -25,8 +25,12 @@ if __name__ == "__main__":
         list_names = list_name.split(".")
         for list_name in list_names[:-1]:
             data = data.setdefault(list_name, {})
-        my_list = data.setdefault(list_names[-1], [])
-        if len(sys.argv) < 2:
+        if list_names[-1]:
+            my_list = data.setdefault(list_names[-1], [])
+        else:
+            # print all
+            my_list = data
+        if len(sys.argv) <= 2:
             cprint(my_list)
         else:
             for arg in sys.argv[2:]:
