fd25b99b17eb7cef88ad1714b3eb0c6b6e60453a
Willem Van Onsem <yourfriends@hapytex.eu>
2025-03-30 13:02:00 +0200

    print measurement dicts

--- add_measure.py
+++ add_measure.py
@@ -5,6 +5,7 @@ import re
 import sys
 from ast import literal_eval
 from datetime import datetime
+from color_pprint import cprint
 
 from dateparser import parse
 from filelock import FileLock
@@ -13,8 +14,9 @@ DURATION = re.compile(r"\d{1,2}([:]\d{2})+")
 ASSET_PATH = "measurements.json"
 
 if __name__ == "__main__":
+    n = len(sys.argv)
     assert (
-        len(sys.argv) > 2
+        n > 1
     ), "You should provide the name of the measurement and the corresponding value"
     with FileLock(f"{ASSET_PATH}.lock"):
         try:
@@ -25,7 +27,7 @@ if __name__ == "__main__":
         dt = datetime.now().replace(microsecond=0).isoformat()
         to_sort = {}
         _def = {}
-        for i in range(1, len(sys.argv) - 1, 2):
+        for i in range(1, n, 2):
             key = sys.argv[i]
             try:
                 key, dt2 = key.rsplit("@", 1)
@@ -35,21 +37,6 @@ if __name__ == "__main__":
             except ValueError:
                 # key is already fine
                 key_dt = dt
-            val = sys.argv[i + 1].strip()
-            if DURATION.fullmatch(val.strip()):
-                d = 0
-                for di in val.split(":"):
-                    d *= 60
-                    d += int(di)
-                val = d
-            else:
-                try:
-                    val = literal_eval(val)
-                except (ValueError, SyntaxError):
-                    try:
-                        val = json.loads(val)
-                    except ValueError:
-                        pass  # keep it a string
             datum = data
             for ky in key.split("."):
                 _datum = datum.setdefault(ky, _def)
@@ -58,10 +45,28 @@ if __name__ == "__main__":
                     # need to resort datum, new entry
                     to_sort[id(datum)] = datum
                 datum = _datum
-            datum[key_dt] = val
-            if key_dt is not dt:
-                # we add a key from the past
-                to_sort[id(datum)] = datum
+            if n > i + 1:
+                val = sys.argv[i + 1].strip()
+                if DURATION.fullmatch(val.strip()):
+                    d = 0
+                    for di in val.split(":"):
+                        d *= 60
+                        d += int(di)
+                    val = d
+                else:
+                    try:
+                        val = literal_eval(val)
+                    except (ValueError, SyntaxError):
+                        try:
+                            val = json.loads(val)
+                        except ValueError:
+                            pass  # keep it a string
+                datum[key_dt] = val
+                if key_dt is not dt:
+                    # we add a key from the past
+                    to_sort[id(datum)] = datum
+            else:
+                cprint(datum)
         for datum in to_sort.values():
             datum_sort = {k: v for k, v in sorted(datum.items())}
             datum.clear()
