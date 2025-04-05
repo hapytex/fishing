237ec7bc549650a339ab713f21cdb1a4702bb9d0
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-05 13:58:48 +0200

    factor out timestamp

--- dated.fish
+++ dated.fish
@@ -1,3 +1,3 @@
 function dated --description 'add the timestamp as a prefix of the given files'
-  rename "s/^/"(date +%Y%m%d%H%M%S)"_/g" $argv
+  rename "s/^/"(timestamp)"_/g" $argv
 end
---
+++ timestamp.fish
@@ -0,0 +1,3 @@
+function timestamp --description 'get a string that specifies date and time, used for filenames mainly'
+  date +%Y%m%d%H%M%S
+end
