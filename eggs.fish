026e2e2d3957730ce61d7ff0b1b0a89fb8a78de4
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-05 16:48:39 +0200

    here is the news

--- eggs.fish
+++ eggs.fish
@@ -2,4 +2,5 @@ function eggs --description 'wait until the eggs are fully cooked in the water b
   measure cooking.eggs.boiled true &
   echo -e 'takes \e[1m5%\e[0m of the home battery'
   waitfor 1500 '🥚 eggs' '🥚 '
+  here_is_the_news
 end
