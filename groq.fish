a1c59296fcacee52586d003a9288ebd559a17fa5
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-06 21:21:19 +0200

    groq -> Groq

--- groq.fish
+++ groq.fish
@@ -1,4 +1,4 @@
-function groq --description 'Ask groq something' -a name
+function groq --description 'Ask Groq something' -a name
   set pth (/usr/bin/pwd)
   cd (assets)
   /usr/bin/python3 ../ask_groq.py (setvar GROQ_API_TOKEN) $argv | quickread
--- summarize.fish
+++ summarize.fish
@@ -1,3 +1,3 @@
-function summarize --description 'Ask groq to summarize the text from the stdin'
+function summarize --description 'Ask Groq to summarize the text from the stdin'
   groq 'you are given a text, try to summarize without losing too much data'
 end
