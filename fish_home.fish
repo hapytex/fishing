5fd11aa2d756d476d828f640a5c2ce90c36e07c0
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-07 13:23:43 +0200

    make fish_home wrappable

--- fish_home.fish
+++ fish_home.fish
@@ -1,3 +1,10 @@
 function fish_home --description 'get the directory where the fish functions are stored'
-  echo $fish_function_path[1]
+  if set -q argv[1]
+    set pth (/usr/bin/pwd)
+    cd $fish_function_path[1]
+    $argv
+    cd "$pth"
+  else
+    echo $fish_function_path[1]
+  end
 end
