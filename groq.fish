cada1bb42fc575e012d3abf6b39fcbd7c2224bb7
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-07 13:22:00 +0200

    make assets wrappable

--- groq.fish
+++ groq.fish
@@ -1,6 +1,3 @@
 function groq --description 'Ask Groq something' -a name
-  set pth (/usr/bin/pwd)
-  cd (assets)
-  /usr/bin/python3 ../ask_groq.py (setvar GROQ_API_TOKEN) $argv | quickread
-  cd "$pth"
+  assets /usr/bin/python3 ../ask_groq.py (setvar GROQ_API_TOKEN) $argv | quickread
 end
--- measure.fish
+++ measure.fish
@@ -1,6 +1,3 @@
 function measure --description 'Add a given measurement at a given time' -a name -a value
-  set pth (/usr/bin/pwd)
-  cd (assets)
-  /usr/bin/python3 ../add_measure.py $argv
-  cd "$pth"
+  assets /usr/bin/python3 ../add_measure.py $argv
 end
