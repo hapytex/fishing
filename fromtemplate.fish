1afd99b253517e8a9483a7e09990723f3c874aaf
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-09 12:56:02 +0200

    fix fromtemplate

--- fromtemplate.fish
+++ fromtemplate.fish
@@ -1,8 +1,7 @@
-function fromtemplate
-    set tmpn "$argv[1]"
+function fromtemplate -a tmpn -a to
     set src "$fish_function_path[1]/templates/$tmpn"
-    set -q "$argv[2]" || set to "$argv[1]" && set to "$argv[2]"
-    natrix-env jinja2 -o "$to" -D "dirname="(basename (pwd)) -D "basename="(basename "$to") -- "$src" || cp "$src" "$to"
+    if set -n "$to" || set to "$tmpn"
+    natrix-env jinja2 -o "$to" -D "dirname="(basename (/usr/bin/pwd)) -D "basename="(basename "$to") -- "$src" || cp "$src" "$to"
     cp --attributes-only -- "$src" "$to"
     git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null && git add "$to"
     editor "$to"
