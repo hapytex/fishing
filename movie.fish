572183ffdd9c1e617b21a9c9e2b85a1170eddcfa
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-11 20:16:00 +0200

    fix typo

--- movie.fish
+++ movie.fish
@@ -1,4 +1,4 @@
 function movie --description 'register going to a movie' -a name
   test -n "$name" || read -P (string unescape '\e[31name of the movie\e[0m> ') name
-  measure entertaiment.movie "$name"
+  measure entertainment.movie "$name"
 end
