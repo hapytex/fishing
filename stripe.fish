7bbd56b5062e43a376f5aa16171906c26bf9dfe5
Willem Van Onsem <yourfriends@hapytex.eu>
2025-04-15 22:37:12 +0200

    stripe colors for white chars

--- stripe.fish
+++ stripe.fish
@@ -1,6 +1,8 @@
 function stripe --description 'add striped background colors when printing content'
   set stripea (echo -en '\e[48;2;221;235;247m\e[30m')
   set stripeb (echo -en '\e[48;2;255;255;255m\e[30m')
+  set stripea (echo -en '\e[48;2;33;15;55m')
+  set stripeb (echo -en '\e[48;2;79;28;81m')
   set stripev (echo -en '\e[0m')
   set eol (echo -en '\e[K\e[0m')
   sed -Ez "s/([^\n]*)\n(([^\n]*)(\n))?/$stripea\1$eol\n$stripeb\3$eol\4/g"
