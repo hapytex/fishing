a46cdaf1738a74d4a64b620346cd70683cb3b008
Willem Van Onsem <yourfriends@hapytex.eu>
2025-05-18 17:19:32 +0200

    extra tasks to ledger

--- ledger.fish
+++ ledger.fish
@@ -1,16 +1,21 @@
 function ledger --description 'book a financial transaction and see the balance' -a am -a to -a nm -a fm -a date -a cy -a rf
   set ass (assets)
+  set task 'balance'
   if set -q argv[1]
-    set nl (echo -e '\n')
-    test -n "$dt" || set dt (date '+%Y/%m/%d')
-    test -n "$am" || read -P (string unescape '\e[31mamount\e[0m> ') am
-    test -n "$cy" || set cy 'E'
-    test -n "$to" || read -P (string unescape '\e[31msource\e[0m> ') to
-    test -n "$fm" || set fm main
-    test -n "$fm" && set fm "\n  $fm"
-    test -n "$rf" && set rf " ($rf)"
-    test -n "$nm" || set nm transaction
-    echo -e "\n$dt$rf * $nm\n  $to  $cy$am$fm" | tee -a "$ass/ledger/transaction.dat"
+    if [ "$argv[1]" = 'budget' ]
+      set task "$argv[1]"
+    else
+      set nl (echo -e '\n')
+      test -n "$dt" || set dt (date '+%Y/%m/%d')
+      test -n "$am" || read -P (string unescape '\e[31mamount\e[0m> ') am
+      test -n "$cy" || set cy 'E'
+      test -n "$to" || read -P (string unescape '\e[31msource\e[0m> ') to
+      test -n "$fm" || set fm main
+      test -n "$fm" && set fm "\n  $fm"
+      test -n "$rf" && set rf " ($rf)"
+      test -n "$nm" || set nm transaction
+      echo -e "\n$dt$rf * $nm\n  $to  $cy$am$fm" | tee -a "$ass/ledger/transaction.dat"
+    end
   end
-  /usr/bin/ledger -f $ass/ledger/ledger.dat balance
+  /usr/bin/ledger -f $ass/ledger/ledger.dat "$task"
 end
