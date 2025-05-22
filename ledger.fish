297d61c28092b6adb01e8290c0cc5b8c051f5a4a
Willem Van Onsem <yourfriends@hapytex.eu>
2025-05-22 21:55:37 +0200

    let ledger account for taxes

--- ledger.fish
+++ ledger.fish
@@ -5,6 +5,12 @@ function ledger --description 'book a financial transaction and see the balance'
     if [ "$argv[1]" = 'budget' ]
       set task "$argv[1]"
     else
+      if string match -r '/' "$am"
+        set ams (string split / -m 1 "$am")
+        set am "$ams[1]"
+        set tx "$ams[2]"
+      else
+      end
       set nl (echo -e '\n')
       test -n "$dt" || set dt (date '+%Y/%m/%d')
       test -n "$am" || read -P (string unescape '\e[31mamount\e[0m> ') am
@@ -14,7 +20,8 @@ function ledger --description 'book a financial transaction and see the balance'
       test -n "$fm" && set fm "\n  $fm"
       test -n "$rf" && set rf " ($rf)"
       test -n "$nm" || set nm transaction
-      echo -e "\n$dt$rf * $nm\n  $to  $cy$am$fm" | tee -a "$ass/ledger/transaction.dat"
+      test -n "$tx" && set tx "\n  tax  $cy$tx"
+      echo -e "\n$dt$rf * $nm\n  $to  $cy$am$tx$fm" | tee -a "$ass/ledger/transaction.dat"
     end
   end
   /usr/bin/ledger -f $ass/ledger/ledger.dat "$task"
