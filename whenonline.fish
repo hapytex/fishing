c5edd075e719bffcd6c928fa8de6878f76e9e3c6
Willem Van Onsem <yourfriends@hapytex.eu>
2025-05-22 21:56:28 +0200

    keep checking for network connection

--- whenonline.fish
+++ whenonline.fish
@@ -1,6 +1,5 @@
 function whenonline --description 'wait to execute a command until the system can somehow contact the outer world'
-  while ! ping -W 0.2 -c 1 8.8.8.8 >/dev/null 2>/dev/null
-    sleep 0.05
+  while ! ping -W 0.25 -c 1 8.8.8.8 >/dev/null 2>/dev/null
   end
   $argv
 end
