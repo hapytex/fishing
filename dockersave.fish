8fbdde6c2786ba26599fdc5996b7f9921a570453
Willem Van Onsem <yourfriends@hapytex.eu>
2025-05-25 11:45:51 +0200

    dockersave pv

--- dockersave.fish
+++ dockersave.fish
@@ -1,6 +1,6 @@
 function dockersave --description 'save a docker image as a compressed tarball' -a name
   set nms (string split ':' -m 1 "$name")
   set target "$nms[1]"
-  docker save "$name" | gzip > "$target.tar.gz"
+  docker save "$name" | pv | gzip > "$target.tar.gz"
   chmod a-w "$target.tar.gz"
 end
