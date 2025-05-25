00b3a220db044f506179afa6469aa553f21e474c
Willem Van Onsem <yourfriends@hapytex.eu>
2025-05-25 18:37:36 +0200

    sequence of dockersave

--- dockersave.fish
+++ dockersave.fish
@@ -1,8 +1,13 @@
-function dockersave --description 'save a docker image as a compressed tarball' -a name
-  set nms (string split ':' -m 1 "$name")
-  set target "$nms[1]"
-  echo -en "\033]0;🐳 dockersave $name\7"
-  set sz (docker image inspect -f '{{ .Size }}' "$name")
-  docker save "$name" | pv -s "$sz" | gzip > "$target.tar.gz"
-  chmod a-w "$target.tar.gz"
+function dockersave --description 'save a docker image as a compressed tarball'
+  set i 1
+  set n (count $argv)
+  for name in $argv
+    set nms (string split ':' -m 1 "$name")
+    set target "$nms[1]"
+    echo -en "\033]0;🐳 $i/$n dockersave $name\7"
+    set sz (docker image inspect -f '{{ .Size }}' "$name")
+    docker save "$name" | pv -s "$sz" | gzip > "$target.tar.gz"
+    chmod a-w "$target.tar.gz"
+    set i (math "$i+1")
+  end
 end
