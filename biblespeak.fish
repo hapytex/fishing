25dbe34f344c5d632eefc5bae2fdedfa330ecc1f
Willem Van Onsem <yourfriends@hapytex.eu>
2025-05-31 02:00:15 +0200

    update biblespeak with entire chapter

--- biblespeak.fish
+++ biblespeak.fish
@@ -1,6 +1,6 @@
 function biblespeak --description 'speak the next verse of the *Bible*'
   set bibleend (math (setvar BIBLE_SPEAK) "+1")
-  # echo ( sed "$BIBLE_SPEAK!d" assets/biblebooks.txt)":1-"(sed "$bibleend!d" assets/biblebooks.txt)":1"
-  bible ( sed "$BIBLE_SPEAK!d" assets/biblebooks.txt)":1-"(sed "$bibleend!d" assets/biblebooks.txt)":1" | sed -E '/^  [0-9]+ /d' | piper
+  set chap (string split , -- (sed "$BIBLE_SPEAK!d" (assets)/bibleverses.csv))
+  bible "$chap[1]:1-$chap[1]:$chap[2]" | sed -E '/^  [0-9]+ /d' | piper
   set -Ux BIBLE_SPEAK $bibleend
 end
