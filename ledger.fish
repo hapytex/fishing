5c9915c96264dc76b011ede8e36c104a16c2014a
Willem Van Onsem <yourfriends@hapytex.eu>
2025-06-16 23:33:09 +0200

    add tip as asset

--- ledger.fish
+++ ledger.fish
@@ -16,7 +16,7 @@ function ledger --description 'book a financial transaction and see the balance'
       test -n "$rf" && set rf " ($rf)"
       test -n "$nm" || set nm transaction
       test -n "$tx" && set tx "\n  tax  $cy$tx"
-      set tos (string split '/' "$to") 'vat' 'ship' 'discount' 'tax'
+      set tos (string split '/' "$to") 'vat' 'ship' 'discount' 'tip' 'tax'
       set tot ''
       set idx 1
       for am in $ams
