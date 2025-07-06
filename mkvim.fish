552314a86ddc670baa2568ddedffb69b70d91003
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-06 22:38:32 +0200

    more save cp/dirname/...

--- mkvim.fish
+++ mkvim.fish
@@ -1,6 +1,6 @@
 function mkvim --description 'Make the directories needed to edit a file'
   for item in $argv
-    mkdir -p (dirname "$item")
-    editor "$item"
+    mkdir -p (dirname -- "$item")
+    editor -- "$item"
   end
 end
