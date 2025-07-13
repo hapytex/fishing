e48395856f41f9933172eae56bd2ff9493385bbf
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-13 18:44:27 +0200

    more traps to quit

--- alarm.fish
+++ alarm.fish
@@ -8,7 +8,7 @@ function alarm --description 'show distress'
   timeout 3600 play -q -n synth 3 sin 960 sine 1000 synth 3 sin fmod 1920 fade l 0 3 2.8 trim 0 1 repeat 3600 &
   set gamma $last_pid
   disown "$gamma"
-  trap "endpom $gamma" EXIT
+  trap "endpom $gamma" EXIT KILL INT QUIT STOP
    while true
     for i in (seq 55 50 255)
       set rr (math "round(237*$i/255)")
