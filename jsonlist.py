f05bdf2dc57262d05e602a09a7a708c313d515a0
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-13 22:11:06 +0200

    jsonlist can also process dicts

--- jsonlist.py
+++ jsonlist.py
@@ -6,7 +6,14 @@ from ast import literal_eval
 
 if __name__ == "__main__":
     lst = []
+    dct = {}
     for val in sys.argv[1:]:
+        key = None
+        if val.startswith('-'):
+            val = val[1:]
+            if val.startswith('-'):
+                val = val[1:]
+            key, val = val.split('=', 1)
         try:
             val = literal_eval(val)
         except (ValueError, SyntaxError):
@@ -14,5 +21,11 @@ if __name__ == "__main__":
                 val = json.loads(val)
             except ValueError:
                 pass  # keep it a string
-        lst.append(val)
-    print(json.dumps(lst))
+        if key is not None:
+            dct[key] = val
+        else:
+            lst.append(val)
+    if dct:
+        print(json.dumps(dct))
+    else:
+        print(json.dumps(lst))
