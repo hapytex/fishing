30e9f8a58eba8ca06eced9ed153f49e7a3b9597b
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-19 08:25:12 +0200

    backup makes item readonly

--- backup.fish
+++ backup.fish
@@ -2,11 +2,11 @@ function backup --description 'make readonly timestamped copies of the list of p
   set dt (timestamp)
   set -l targets
   for item in $argv;
-    cp "$item" $dt"_$item"
+    cp -- "$item" $dt"_$item"
     set -a targets $dt"_$item"
   end
   if set -q targets[1]
-    chmod a-w $targets
-    chattr +c $targets
+    chmod a-w -- $targets
+    chattr +c -- $targets
   end
 end
