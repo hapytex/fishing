f065620ef771bc39e45595743a30e538042f63a2
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-20 15:30:27 +0200

    truthiness checks

--- assets.fish
+++ assets.fish
@@ -3,7 +3,9 @@ function assets --description 'get the directory where the fish assets are store
     set pth (/usr/bin/pwd)
     cd (fish_home)/assets/
     $argv
+    set st $status
     cd "$pth"
+    return $st
   else
     echo (fish_home)/assets/
   end
---
+++ truthful.fish
@@ -0,0 +1,3 @@
+function truthful --description 'Check if at least one of the given Python literals or JSON values has turthiness true'
+  fish_home natrix truthful.py $argv
+end
