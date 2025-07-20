f74eba5c2c46169211308030a1f94e176db33778
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-20 08:15:15 +0200

    write iso read

--- vitamins.fish
+++ vitamins.fish
@@ -1,5 +1,5 @@
 function vitamins --description 'take our daily vitamines and magnesium'
   waitfor 90 '💊 vitamins' '💊 '
-  measurelog health.vitamins
+  measurelog health.vitamins true
   hydrate 33cl
 end
