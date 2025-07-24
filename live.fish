bfcd8d91cf1bbb0edf5c1d6ef3bcd2a483ee5554
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-24 19:26:43 +0200

    use one echo, not two

--- live.fish
+++ live.fish
@@ -1,8 +1,9 @@
 function live --description 'run a command every second and update the result in the console'
+  set pre ''
   while true
     set data ($argv | string split0)
-    echo "$data"
+    echo "$pre$data"
     sleep 1
-    echo -en '\e['(echo "$data" | wc -l)'A'
+    set pre (echo -en '\e['(echo "$data" | wc -l)'A')
   end
 end
