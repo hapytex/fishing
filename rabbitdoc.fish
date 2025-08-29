79482690a3c3b9e67f8ed0838acedda260e0b79c
Willem Van Onsem <yourfriends@hapytex.eu>
2025-08-29 20:36:35 +0200

    open browser to issue

--- rabbitdoc.fish
+++ rabbitdoc.fish
@@ -1,3 +1,3 @@
 function rabbitdoc --description 'ask CodeRabbit.ai to generate docstrings for the latest pull request'
-  gh pr comment (gh pr list --json number --jq '.[0].number') -b '@coderabbitai generate docstrings'
+  firefox (gh pr comment (gh pr list --json number --jq '.[0].number') -b '@coderabbitai generate docstrings')
 end
