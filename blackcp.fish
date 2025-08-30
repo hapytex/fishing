528c4de65305faf85686b0b4f216878484bcdda6
Willem Van Onsem <yourfriends@hapytex.eu>
2025-08-30 11:54:30 +0200

    blackcp

--- blackcp.fish
+++ blackcp.fish
@@ -1,5 +1,10 @@
 function blackcp --description 'run the Black formatter, and if Black made changes, commit and push these'
-  if ! black --check --quiet $argv
+  if set -q argv[1]
+    set args . $argv
+  else
+    set args $argv
+  fi
+  if ! black --check --quiet $args
     black $argv
     gitcp 'black reformatting'
   end
