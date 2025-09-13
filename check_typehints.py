ccb916a8b53e347943889d094446378455f5a1db
Willem Van Onsem <yourfriends@hapytex.eu>
2025-09-13 23:03:27 +0200

    exit code for check_typehints

--- check_typehints.py
+++ check_typehints.py
@@ -21,6 +21,12 @@ class CheckTypeHints(ast.NodeTransformer):
     def __init__(self, *args, filename="<unknown>", **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.filename = filename
+        self.errors = 0
+
+    def report(self, node, message):
+        location = self.get_qualfied_name(node)
+        print(f'{location}: {message} has no typehint')
+        self.errors += 1
 
     def get_qualfied_name(self, node: ast.AST):
         return f"{self.filename}:{node.lineno}"
@@ -38,11 +44,9 @@ class CheckTypeHints(ast.NodeTransformer):
                         not argi.annotation
                         and argi.arg not in self.IGNORE_PARAMETER_NAMES
                     ):
-                        print(
-                            f"{location}: argument \x1b[1m{argi.arg}\x1b[0m has no type hint"
-                        )
+                        self.report(node, f'argument \x1b[1m{argi.arg}\x1b[0m')
         if not node.returns:
-            print(f"{location}: no return type hint")
+            self.report(node, f"the return type")
         return self.generic_visit(node)
 
     def visit_FunctionDef(self, node):
@@ -53,8 +57,12 @@ class CheckTypeHints(ast.NodeTransformer):
 
 
 if __name__ == "__main__":
+    errors = 0
     for file in sys.argv[1:]:
         converter = CheckTypeHints(filename=file)
         with open(file, "rb") as f:
             tree = ast.parse(f.read(), filename=file)
         converter.visit(tree)
+        errors += converter.errors
+    if errors:
+        exit(1 + (errors % 255))
