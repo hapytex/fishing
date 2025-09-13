5d8c1282a280c1e56a731d960188b3e96737936e
Willem Van Onsem <yourfriends@hapytex.eu>
2025-09-13 14:48:34 +0200

    include copy of chmod

--- fromtemplate.fish
+++ fromtemplate.fish
@@ -3,6 +3,7 @@ function fromtemplate -a tmpn -a to
     test -n "$to" || set to "$tmpn"
     natrix-env jinja2 -o "$to" -D "dirname="(basename (/usr/bin/pwd)) -D "basename="(basename "$to") -- "$src" || cp "$src" "$to"
     cp --attributes-only -- "$src" "$to"
+    chmod (stat --format '%a' -- "$src") -- "$to"
     git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null && git add "$to"
     editor "$to"
 end
