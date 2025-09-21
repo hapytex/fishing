cd3f541e69c1ea295b5d62a572fb2d50970f9866
Willem Van Onsem <yourfriends@hapytex.eu>
2025-09-21 16:48:28 +0200

    mkdir --

--- vimgit.fish
+++ vimgit.fish
@@ -1,6 +1,6 @@
 function vimgit --description 'Edit the given files in the parameters, and add these to git'
   for item in $argv
-    mkdir -p (dirname -- "$item")
+    mkdir -p -- (dirname -- "$item")
     editor -- "$item"
     git add -- "$item"
   end
