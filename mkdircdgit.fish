c53e4d1f46c8a429d714b7fcd26031b37a36b293
Willem Van Onsem <yourfriends@hapytex.eu>
2025-10-25 20:22:49 +0200

    copy gitignores

--- mkdircdgit.fish
+++ mkdircdgit.fish
@@ -1,3 +1,9 @@
-function mkdircdgit --description 'make a directory, if it does not yet exist, cd into that directory, and start a git repository'
-  mkdircd $argv && git init .
+function mkdircdgit --description 'make a directory, if it does not yet exist, cd into that directory, and start a git repository' -a pth -a lang
+  mkdircd "$pth" $argv && git init .
+  if [ -n "$lang" ]
+    set gt (fish_home)/gitignores/$lang.gitignore
+    if [ -e "$gt" ]
+      cp "$gt" .gitignore
+    end
+  end
 end
