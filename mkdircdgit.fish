9e859088de3089dfaf3abbb5a00c474870b83edf
Willem Van Onsem <yourfriends@hapytex.eu>
2025-10-25 20:32:20 +0200

    add gitignore to git

--- mkdircdgit.fish
+++ mkdircdgit.fish
@@ -2,8 +2,9 @@ function mkdircdgit --description 'make a directory, if it does not yet exist, c
   mkdircd "$pth" $argv && git init .
   if [ -n "$lang" ]
     set gt (fish_home)/gitignores/$lang.gitignore
-    if [ -e "$gt" ]
+    if [ -e "$gt" -a ! -e .gitignore ]
       cp "$gt" .gitignore
+      git add .gitignore
     end
   end
 end
