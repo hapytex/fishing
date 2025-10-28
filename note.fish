7012d3013ce2c7f1716140791c12bdb22ac4a53f
Willem Van Onsem <yourfriends@hapytex.eu>
2025-10-28 21:03:50 +0100

    empty notes not saved

--- note.fish
+++ note.fish
@@ -5,7 +5,11 @@ function note --description 'Add a notebook entry'
     set hhmm (date +%H%M)
     set nt (read -P (echo -e "\033]0;📓 $hhmm Notebook entry\a  \e[93m$hhmm>\e[0m "))
     set hhmm (date +%H%M)
-    echo -e "\033[1A\r\e[32m✓ $hhmm>\e[0m"
+    if [ -n "$nt" ]
+      echo -e "\e[1A\r\e[32m✓ $hhmm>\e[0m"
+    else
+      echo -e "\e[1A\r\e[31m✗ $hhmm>\e[0m"
+    end
   end
   if [ -n "$nt" ]
     measurelog notebook "$nt"
