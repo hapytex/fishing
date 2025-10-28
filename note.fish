2a2926676692f79e663ff694c9df86cda57ef72c
Willem Van Onsem <yourfriends@hapytex.eu>
2025-10-28 20:18:53 +0100

    visual marking for notebooks

--- note.fish
+++ note.fish
@@ -2,7 +2,10 @@ function note --description 'Add a notebook entry'
   if set -q argv[1]
     set nt (echo $argv)
   else
-    set nt (read -P (date '+%H%M> '))
+    set hhmm (date +%H%M)
+    set nt (read -P (echo -e "  \e[93m$hhmm>\e[0m"))
+    set hhmm (date +%H%M)
+    echo -e "\033[1A\r\e[32m✓ $hhmm>\e[0m"
   end
   if [ -n "$nt" ]
     measurelog notebook "$nt"
