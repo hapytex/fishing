a29b99222379e8594fa1fd2ed7754a5259cb94f9
Willem Van Onsem <yourfriends@hapytex.eu>
2025-11-02 21:22:36 +0100

    faster setvar

--- setvar.fish
+++ setvar.fish
@@ -1,5 +1,7 @@
 function setvar --description 'check if a variable exists, and otherwise prompt for a value' -a name -a helptext
-  test -n "$helptext" && set helptext " [$helptext]"
-  set -q "$name" || read -Ux "$name" -P (string unescape '$\e[31m')"$name$helptext"(string unescape '\e[0m> ')
+  if ! set -q name
+      test -n "$helptext" && set helptext " [$helptext]"
+      read -Ux "$name" -P (string unescape '$\e[31m')"$name$helptext"(string unescape '\e[0m> ')
+  end
   eval "echo \"\$$name\""
 end
