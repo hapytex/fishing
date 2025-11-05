26b9ab67312fe0adf05f74f68a3040499fc6bfe8
Willem Van Onsem <yourfriends@hapytex.eu>
2025-11-05 21:49:11 +0100

    heal colors

--- facialmask.fish
+++ facialmask.fish
@@ -1,5 +1,5 @@
 function facialmask --description 'register the use of a facial mask' -a kind
   measurelog health.facialmask "$kind" &
-  waitfor 1200 '' '🧖 facial mask' '🧖 '
+  waitfor 1200 '' '🧖 facial mask' '🧖 ' (getcolor random heal_colors ,)
   here_is_the_news
 end
