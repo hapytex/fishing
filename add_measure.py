ecdd0cbaa5f8749c76513abeb97e9fa02be7c56f
Willem Van Onsem <yourfriends@hapytex.eu>
2025-11-09 15:48:32 +0100

    move to sqlite based db for measurements

--- add_measure.py
+++ add_measure.py
@@ -6,31 +6,25 @@ import sys
 from ast import literal_eval
 from datetime import datetime
 from shutil import move
+import sqlite3
+from peewee import DateTimeField, Model, SqliteDatabase, TextField
 
 from color_pprint import cprint
 from dateparser import parse
 from filelock import FileLock
 
 DURATION = re.compile(r"\d{1,2}([:]\d{2})+")
-ASSET_PATH = "assets/measurements.json"
+ASSET_PATH = "assets/measurements.db"
 
+db = SqliteDatabase(ASSET_PATH)
 
-def filter_dict(data, dt):
-    if isinstance(data, dict):
-        result = {}
-        for k, v in data.items():
-            v = filter_dict(v, dt)
-            if v:
-                try:
-                    dt2 = datetime.fromisoformat(k)
-                except ValueError:
-                    result[k] = v
-                else:
-                    if dt2 >= dt:
-                        result[k] = v
-        return result
-    return data
+class Measurement(Model):
+    key = TextField()
+    value = TextField()
+    created_date = DateTimeField(default=datetime.now)
 
+    class Meta:
+        database = db
 
 if __name__ == "__main__":
     n = len(sys.argv)
@@ -38,34 +32,21 @@ if __name__ == "__main__":
         n > 1
     ), "You should provide the name of the measurement and the corresponding value"
     with FileLock(f"{ASSET_PATH}.lock"):
-        try:
-            with open(ASSET_PATH, "r") as f:
-                data = json.load(f)
-        except IOError:
-            data = {}
-        timestamp = datetime.now().replace(microsecond=0)
+        db.connect()
+        db.create_tables([Measurement])
+        key_dt = timestamp = datetime.now()
         dt = timestamp.isoformat()
-        to_sort = {}
-        _def = {}
         for i in range(1, n, 2):
             key = sys.argv[i]
             try:
                 key, dt2 = key.rsplit("@", 1)
-                timefilter = dt2 = parse(dt2)
+                timefilter = dt2 = parse(dt2).replace(tzinfo=None)
                 if dt2 is not None:
-                    key_dt = dt2.replace(microsecond=0, tzinfo=None).isoformat()
+                    key_dt = dt2.replace(tzinfo=None)
             except ValueError:
                 # key is already fine
                 timefilter = datetime.min
                 key_dt = dt
-            datum = data
-            for ky in key.split("."):
-                _datum = datum.setdefault(ky, _def)
-                if _datum is _def:
-                    _def = {}  # new one
-                    # need to resort datum, new entry
-                    to_sort[id(datum)] = datum
-                datum = _datum
             if n > i + 1:
                 val = sys.argv[i + 1].strip()
                 if DURATION.fullmatch(val.strip()):
@@ -83,16 +64,9 @@ if __name__ == "__main__":
                         except ValueError:
                             pass  # keep it a string
                 if val:  # None, False, etc. are all omitted
-                    datum[key_dt] = val
-                    if key_dt is not dt:
-                        # we add a key from the past
-                        to_sort[id(datum)] = datum
+                    Measurement.create(created_date=key_dt, key=key, value=json.dumps(val))
             else:
-                cprint(filter_dict(datum, timefilter))
-        for datum in to_sort.values():
-            datum_sort = {k: v for k, v in sorted(datum.items())}
-            datum.clear()
-            datum.update(datum_sort)
-        with open(f"{ASSET_PATH}.tmp", "w") as f:
-            json.dump(data, f, indent=4)
-        move(f"{ASSET_PATH}.tmp", ASSET_PATH)
+                datum = {}
+                for entry in Measurement.select().where(Measurement.key == key, Measurement.created_date >= timefilter.isoformat()):
+                    datum[parse(entry.created_date).replace(microsecond=0)] = json.loads(entry.value)
+                cprint(datum)
