c21d80fd7c621074327c85a3c1354012136b53ae
Willem Van Onsem <yourfriends@hapytex.eu>
2025-11-11 08:34:21 +0100

    keycolor

--- coffee.fish
+++ coffee.fish
@@ -2,7 +2,7 @@ function coffee --description 'wait until the coffee machine has finished two cu
   set hr (date +%H%M)
   if test \( (setvar COFFEE_MIN) -le "$hr" \) -a \( "$hr" -le (setvar COFFEE_MAX) \)
     measurelog home.coffeemachine.brew true &
-    household 270 '☕☕' 'coffee' 3 0.5
+    household 270 '☕☕' 'coffee' 3 0.5 coffee
   else
     echo >&2 'no coffee anymore'
     return 1
--- household.fish
+++ household.fish
@@ -1,4 +1,7 @@
-function household --description 'wait until a household device has finished with info' -a time -a emoji -a name -a energy -a water
+function household --description 'wait until a household device has finished with info' -a time -a emoji -a name -a energy -a water -a keycol
+  if [ -n "$keycol" ]
+    keycolor (getcolor random "$keycol") 255
+  end
   echo -e "takes \e[1m$energy%\e[0m of the home battery and \e[1m"$water"ℓ\e[0m water"
   waitfor $time '' "$emoji  $name" "$emoji "
   here_is_the_news
