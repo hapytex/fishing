d7bbf11e18d4da247c47719fc0d4c45c207c0625
Willem Van Onsem <yourfriends@hapytex.eu>
2025-11-11 22:57:27 +0100

    parameterize language

--- piper.fish
+++ piper.fish
@@ -1,3 +1,4 @@
-function piper
-  natrix -m piper -m en_US-lessac-medium --output-raw 2>/dev/null | aplay -r 22050 -f S16_LE -t raw - 2>/dev/null
+function piper -a lang
+  test -n "$lang" || set lang en_US-lessac-high
+  natrix -m piper -m "$lang" --output-raw 2>/dev/null | aplay -r 22050 -f S16_LE -t raw - 2>/dev/null
 end
